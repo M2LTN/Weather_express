5d7b37b2269993647f02f87285c6b0a65dbb881c
aaron <aaron.malton@voco.ee>
2024-11-13 13:38:34 +0200

    added basic files

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-import express from 'express';
-const app = express();
+const express = require('express')
+const app = express()
+
+const path = require('path')
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
 
 app.get('/', function(req, res) {
-    res.send('Hello World');
+    res.render('index')
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
+app.listen(3000)
