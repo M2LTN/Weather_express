100c79a1f1d361ee68e254b56e6615d36d201a36
M2LTN <aaron.malton@voco.ee>
2025-02-12 09:58:29 +0200

    Create form request responce with API data

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const path = require('path');
 
 const app = express();
 
+app.use(express.json())
+app(express.urlencoded({extended:true}))
+
 const key = "5599644fba2b60567e2d89ac195babe9"; 
 let city = "Tartu"; 
 
@@ -36,6 +39,24 @@ app.get('/', async (req, res) => {
         });
     }
 });
+
+app.post("/", (req,res) => {
+    let city = req.body.cityname
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
+    .then((response) => {
+        return response.json()
+    })
+    .then((data) => {
+        let description = data.weather[0].description
+        let city = data.name
+        let temp = Math.round(parseFloat(data.main.temp) - 273.15)
+        res.render('index', {
+            description: description,
+            city: city,
+            temp: temp
+        })
+    })
+})
 
 // Start the server
 app.listen(3000, () => {
